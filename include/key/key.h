3aafe7db5170b126b2788d439cb922838cd1f949
Dragon <33776693+ExplodingDragon@users.noreply.github.com>
2021-11-11 23:27:42 +0800

    矩阵按键

--- include/key/key.h
+++ include/key/key.h
@@ -5,5 +5,11 @@
 #ifndef LEARN_C51_KEY_H
 #define LEARN_C51_KEY_H
 
+/**
+ * 矩阵按键选择
+ *
+ * @return 选择的按键，如果未选择则返回 0
+ */
 int selectMatrixKey();
+
 #endif //LEARN_C51_KEY_H
--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
 #include "lcd/LCD1602.h"
 #include "key/key.h"
-#include "utils//delay.h"
+#include "utils/delay.h"
 
 
 int main() {
     int num = 0;
     LCD_Init();
-    LCD_ShowString(1,1,"Select:");
+    LCD_ShowString(1, 1, "Input:");
     while (1) {
 
         int select = selectMatrixKey();
-        if(select!=0){
+        if (select != 0) {
             num = select;
         }
         LCD_ShowNum(1, 10, num, 2);
