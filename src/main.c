b35accf652bb6340d98887a765ce055911e25822
Dragon <33776693+ExplodingDragon@users.noreply.github.com>
2021-11-12 20:42:20 +0800

    时钟

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <c52/regx52.h>
+#include <stdbool.h>
 #include "lcd/LCD1602.h"
 #include "key/key.h"
 #include "utils/delay.h"
@@ -6,32 +7,53 @@
 
 unsigned int count = 0;
 
-unsigned int indexInt = 0;
+
+int h = 23, m = 59, s = 55;
+
+void drawTime() {
+    displayNum(0, h / 10 % 10);
+    displayNum(1, h % 10);
+
+    displayNum(3, m / 10 % 10);
+    displayNum(4, m % 10);
+
+    displayNum(6, s / 10 % 10);
+    displayNum(7, s % 10);
+}
 
 int main() {
     Time0_Init();
     while (1) {
-
+        drawTime();
     }
     return 0;
 }
+
 
 void timer0_routine()
 
 __interrupt (1) {
 //void timer0_routine() {
+    TH0 = 0xFC;
+    TL0 = 0x18;
+    count++;
+    if (count >= 1000) {
+        count = 0;
+        s++;
+        if (s == 60) {
+            s = 0;
+            m++;
+        }
+        if (m == 60) {
+            m = 0;
+            h++;
+        }
 
-TH0 = 0xFC;
-TL0 = 0x18;
-count++;
-if (count >= 500) {
-count = 0;
-
-P2 = (0b00000001 << indexInt) ^ 0xFF;
-indexInt++;
-if (indexInt > 7) {
-indexInt = 0;
-}
-}
+        if (h == 24) {
+            h = 0;
+            m = 0;
+            s = 0;
+        }
+    }
 }
 
